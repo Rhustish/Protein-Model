3e145dc70d04a66e668acc6d7f6185cfb69c24a1
Rhustish <bbhatnagar27@gmail.com>
2023-07-05 02:31:57 +0530

    switched to static serving

--- server.js
+++ server.js
@@ -6,24 +6,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const app = express();
 
+app.use(express.static(__dirname))
 app.get("/",(req,res)=>{
     res.sendFile(`${__dirname}/index.htm`)
-})
-
-app.get("/style.css",(req,res)=>{
-    res.sendFile(`${__dirname}/style.css`)
-})
-
-app.get("/index.js",(req,res)=>{
-    res.sendFile(`${__dirname}/index.js`)
-})
-
-app.get("/logo.svg",(req,res)=>{
-    res.sendFile(`${__dirname}/logo.svg`)
-})
-
-app.get("/slogo.png",(req,res)=>{
-    res.sendFile(`${__dirname}/slogo.png`)
 })
 
 app.listen(3000,()=>{console.log("Server Started on port 3000");})
